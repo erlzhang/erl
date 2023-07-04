1be4db4ba4c901f1c3bb5e15e1b6d34036830907
erlzhang <zhangshiyu1992@hotmail.com>
2023-07-04 10:20:13 +0800

    fix error

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,11 +50,21 @@ module.exports = {
       __key: "pages",
     },
     require.resolve(`./plugins/source-books`),
-       gtagConfig: {
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-G8WW41ZF4X", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
           optimize_id: "G-G8WW41ZF4X",
           anonymize_ip: true,
           cookie_expires: 0,
         },
-  },
+      },
+    },
   ],
 };
