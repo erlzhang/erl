a6070890b3ba5e3cd00f394449f6f24aebefee60
erlzhang <zhangshiyu1992@hotmail.com>
2023-07-13 16:51:05 +0800

    add link to corner

--- src/components/social.js
+++ src/components/social.js
@@ -10,6 +10,12 @@ export default function({ site }) {
             <a href={`https://github.com/${site.github}`} className="icon__link" target="_blank">
                 <Github></Github>
             </a>
+            <a
+              href="https://corner.erl.im"
+              className="text__link"
+              title="一些日常吐槽的地方，可能随时会关闭"
+              target="_blank"
+            >角落</a>
         </div>
     )
 }
